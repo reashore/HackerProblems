0b24947f0d1539b6f3ff426ff4aa69e316cbf1d3
Frank J. Reashore <reashore@telus.net>
2019-12-13 13:20:20 -0800

    Solved CamelCase problem

---
+++ CamelCaseProblem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CamelCaseProblem
+{
+    internal static class Program
+    {
+        internal static void Main()
+        {
+            const string camelCaseString = "saveChangesInTheEditor";
+            int wordCount = CamelCase(camelCaseString);
+            Console.WriteLine($"wordCount = {wordCount}");
+        }
+
+        private static int CamelCase(string s)
+        {
+            int numWords = 1;
+
+            foreach (char c in s)
+            {
+                if (char.IsUpper(c))
+                {
+                    numWords++;
+                }
+            }
+
+            return numWords;
+        }
+    }
+}
