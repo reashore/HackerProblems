7fd1fd9a388deb129229802e8b5604960eb29103
Frank J. Reashore <reashore@telus.net>
2019-12-14 12:16:17 -0800

    Solved VirtualAdvertising problem

---
+++ ViralAdvertisingProblem/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ViralAdvertisingProblem
+{
+    internal static class Program
+    {
+        internal static void Main()
+        {
+            int days = 3;
+            int totalLikes = ViralAdvertising(days);                   // 9
+            Console.WriteLine($"totalLikes = {totalLikes}");
+
+            days = 5;
+            totalLikes = ViralAdvertising(days);                       // 24
+            Console.WriteLine($"totalLikes = {totalLikes}");
+        }
+
+        private static int ViralAdvertising(int days)
+        {
+            const int numberInitialShares = 5;
+            const int newSharesForEachLike = 3;
+            int numberShares = numberInitialShares;
+            int totalLikes = 0;
+            int day = 1;
+
+            while (true)
+            {
+                int numberLikes = numberShares / 2;
+                totalLikes += numberLikes;
+
+                if (day == days)
+                {
+                    break;
+                }
+
+                numberShares = numberLikes * newSharesForEachLike;
+                day++;
+            }
+
+            return totalLikes;
+        }
+    }
+}
