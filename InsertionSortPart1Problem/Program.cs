65cdb1d400eee1714fb97b07d9e583103688565e
Frank J. Reashore <reashore@telus.net>
2019-12-13 16:50:24 -0800

    Solved InsertionSort1 problem

---
+++ InsertionSortPart1Problem/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace InsertionSortPart1Problem
+{
+    internal static class Program
+    {
+        internal static void Main()
+        {
+            int n = -1;
+            int[] array;
+
+            array = new[] { 2, 4, 6, 8, 3 };
+            InsertionSort1(n, array);
+            Console.WriteLine();
+
+            array = new[] { 1, 3, 5, 9, 13, 22, 27, 35, 46, 51, 55, 83, 87, 23 };
+            InsertionSort1(n, array);
+            Console.WriteLine();
+
+            array = new[] { 2, 3, 4, 5, 6, 7, 8, 9, 10, 1 };
+            InsertionSort1(n, array);
+            Console.WriteLine();
+
+            //array = new[] {2, 3, 1};
+            //InsertionSort1(n, array);
+            //Console.WriteLine();
+        }
+
+        private static void InsertionSort1(int length, int[] array)
+        {
+            int len = array.Length;
+            int valueToInsert = array[len - 1];
+            bool insertedValue = false;
+
+            for (int n = len - 1; 0 < n; n--)
+            {
+                if (valueToInsert < array[n - 1])
+                {
+                    array[n] = array[n - 1];
+                }
+                else
+                {
+                    array[n] = valueToInsert;
+                    insertedValue = true;
+                }
+
+                Print(array);
+
+                if (insertedValue)
+                {
+                    break;
+                }
+            }
+
+            if (!insertedValue)
+            {
+                array[0] = valueToInsert;
+                Print(array);
+            }
+        }
+
+        private static void Print(int[] array)
+        {
+            foreach (int element in array)
+            {
+                Console.Write($"{element} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
