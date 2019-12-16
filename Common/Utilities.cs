9e10921e636de1d800ff45d998b48471ae2f0c94
Frank J. Reashore <reashore@telus.net>
2019-12-16 12:18:49 -0800

    Added library for common code

---
+++ Common/Utilities.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Common
+{
+    public static class Utilities
+    {
+        private static string[] ReadData(string fileName)
+        {
+            string location = Assembly.GetExecutingAssembly().Location;
+            string rootDirectory = Path.GetDirectoryName(location);
+            string fullFileName = Path.Combine(rootDirectory, fileName);
+            string[] fileLines = File.ReadAllLines(fullFileName);
+
+            return fileLines;
+        }
+    }
+}
