a6d3e48a86ce86272d84e8d9e450965ff8fcdfa3
Frank J. Reashore <reashore@telus.net>
2019-12-17 00:12:32 -0800

    Solved FunnyString problem

---
+++ AnagramProblem/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnagramProblem
+{
+    internal static class Program
+    {
+        internal static void Main()
+        {
+            //bool areAnagrams = AreAnagrams("xy", "yx");
+
+            string inputString;
+            int numberCharactersToChange;
+
+            //inputString = "aaabbb";
+            //numberCharactersToChange = Anagram(inputString);                                          // 3
+            //Console.WriteLine($"numberCharactersToChange = {numberCharactersToChange}");
+
+            //inputString = "ab";
+            //numberCharactersToChange = Anagram(inputString);                                              // 1
+            //Console.WriteLine($"numberCharactersToChange = {numberCharactersToChange}");
+
+            //inputString = "abc";
+            //numberCharactersToChange = Anagram(inputString);                                              // -1
+            //Console.WriteLine($"numberCharactersToChange = {numberCharactersToChange}");
+
+            //inputString = "mnop";
+            //numberCharactersToChange = Anagram(inputString);                                              // 2
+            //Console.WriteLine($"numberCharactersToChange = {numberCharactersToChange}");
+
+            //inputString = "xyyx";
+            //numberCharactersToChange = Anagram(inputString);                                              // 0
+            //Console.WriteLine($"numberCharactersToChange = {numberCharactersToChange}");
+
+            inputString = "xaxbbbxx";
+            numberCharactersToChange = Anagram(inputString);                                              // -1
+            Console.WriteLine($"numberCharactersToChange = {numberCharactersToChange}");
+        }
+
+        // todo not working for last test case
+        private static int Anagram(string inputString)
+        {
+            int len = inputString.Length;
+            bool oddLength = len % 2 != 0;
+
+            if (oddLength)
+            {
+                return -1;
+            }
+
+            string part1 = inputString.Substring(0, len / 2);
+            string part2 = inputString.Substring(len/2, len / 2);
+
+            if (AreAnagrams(part1, part2))
+            {
+                return 0;
+            }
+
+            Dictionary<char, int> dictionary1 = GetOccurancesDictionary(part1);
+            Dictionary<char, int> dictionary2 = GetOccurancesDictionary(part2);
+
+            int numberChanges = GetNumberChanges(dictionary1, dictionary2);
+
+            return numberChanges;
+        }
+
+        private static bool AreAnagrams(string part1, string part2)
+        {
+            // contain the same characters in dictionary and the respective occurance counts are equal
+
+            Dictionary<char, int> dictionary1 = GetOccurancesDictionary(part1);
+            Dictionary<char, int> dictionary2 = GetOccurancesDictionary(part2);
+
+            if (dictionary1.Count != dictionary2.Count)
+            {
+                return false;
+            }
+
+            bool areAnagrams = true;
+
+            foreach (char c in dictionary1.Keys)
+            {
+                bool condition = dictionary2.ContainsKey(c) && dictionary1[c] == dictionary2[c];
+
+                if (!condition)
+                {
+                    areAnagrams = false;
+                    break;
+                }
+            }
+
+            return areAnagrams;
+        }
+
+        private static int GetNumberChanges(Dictionary<char, int> dictionary1, Dictionary<char, int> dictionary2)
+        {
+            int numberChanges = 0;
+
+            while (true)
+            {
+                int count1 = dictionary1.Count;
+                int count2 = dictionary2.Count;
+
+                if (count1 == 0 && count2 == 0)
+                {
+                    break;
+                }
+
+                if (count1 != count2)
+                {
+                    numberChanges = -1;
+                    break;
+                }
+
+                // removes maxOccurannce key/value
+                int maxOccurannce1 = GetMaxOccurance(dictionary1);
+                int maxOccurannce2 = GetMaxOccurance(dictionary2);
+
+                if (maxOccurannce1 != maxOccurannce2)
+                {
+                    numberChanges = -1;
+                    break;
+                }
+
+                numberChanges += maxOccurannce1;
+            }
+
+            return numberChanges;
+        }
+
+        private static int GetMaxOccurance(Dictionary<char, int> dictionary)
+        {
+            int maxOccurance = 0;
+            char maxCharacter = char.MinValue;
+
+            if (dictionary.Count == 0)
+            {
+                return -1;
+            }
+
+            foreach (char character in dictionary.Keys)
+            {
+                int occurance = dictionary[character];
+
+                if (occurance > maxOccurance)
+                {
+                    maxOccurance = occurance;
+                    maxCharacter = character;
+                    break;
+                }
+            }
+
+            // note destructive operation
+            dictionary.Remove(maxCharacter);
+
+            return maxOccurance;
+        }
+
+        private static Dictionary<char, int> GetOccurancesDictionary(string s)
+        {
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+
+            foreach (char c in s)
+            {
+                if (!dictionary.ContainsKey(c))
+                {
+                    dictionary[c] = 1;
+                }
+                else
+                {
+                    dictionary[c]++;
+                }
+            }
+
+            return dictionary;
+        }
+    }
+}
---
+++ FunnyStringProblem/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+
+namespace FunnyStringProblem
+{
+    internal static class Program
+    {
+        internal static void Main()
+        {
+            var inputString = "acxz";
+            var result = FunnyString(inputString);      // Funny
+            Console.WriteLine($"result = {result}");
+
+            inputString = "bcxz";
+            result = FunnyString(inputString);                // Not Funny
+            Console.WriteLine($"result = {result}");
+
+            inputString = "lmnop";
+            result = FunnyString(inputString);                // Funny
+            Console.WriteLine($"result = {result}");
+        }
+
+        private static string FunnyString(string inputString)
+        {
+            int len = inputString.Length;
+            string reversedString = Reverse(inputString);
+            bool differenceStringsAreEqual = true;
+
+            for (int n = 0; n < len / 2; n++)
+            {
+                char first1 = inputString[n];
+                char next1 = inputString[n + 1];
+                int difference1 = Math.Abs(first1 - next1);
+
+                char first2 = reversedString[n];
+                char next2 = reversedString[n + 1];
+                int difference2 = Math.Abs(first2 - next2);
+
+                if (difference1 != difference2)
+                {
+                    differenceStringsAreEqual = false;
+                    break;
+                }
+            }
+
+            return differenceStringsAreEqual ? "Funny" : "Not Funny";
+        }
+
+        private static string Reverse(string s)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int len = s.Length;
+
+            for (int n = len - 1; 0 <= n; n--)
+            {
+                stringBuilder.Append(s[n]);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
