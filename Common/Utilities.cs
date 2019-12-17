ff24ace7702fec90fae6b9b4c1708b386a9d3c6f
Frank J. Reashore <reashore@telus.net>
2019-12-17 08:48:24 -0800

    Solved Gemstones problem

--- Common/Utilities.cs
+++ Common/Utilities.cs
@@ -1,11 +1,13 @@
 ﻿using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Text;
 
 namespace Common
 {
     public static class Utilities
     {
-        private static string[] ReadTextFile(string fileName)
+        public static string[] ReadTextFile(string fileName)
         {
             string location = Assembly.GetExecutingAssembly().Location;
             string rootDirectory = Path.GetDirectoryName(location);
@@ -13,6 +15,24 @@ namespace Common
             string[] fileLines = File.ReadAllLines(fullFileName);
 
             return fileLines;
+        }
+
+        public static string Reverse(string s)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int len = s.Length;
+
+            for (int n = len - 1; 0 <= n; n--)
+            {
+                stringBuilder.Append(s[n]);
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public static string GetDistinctChars(string inputString)
+        {
+            return new string(inputString.Distinct().ToArray());
         }
     }
 }
---
+++ GemstonesProblem/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common;
+
+namespace GemstonesProblem
+{
+    internal static class Program
+    {
+        internal static void Main()
+        {
+            string[] array = {"abcdde", "baccd", "eeabg"};
+            int numberGemstones = Gemstones(array);
+            Console.WriteLine($"numberGemstones = {numberGemstones}");
+        }
+
+        private static int Gemstones(string[] array)
+        {
+            int len = array.Length;
+            List<string> distinctList = new List<string>();
+            int numberGemstones = 0;
+
+            foreach (string s in array)
+            {
+                string distinct = Utilities.GetDistinctChars(s);
+                distinctList.Add(distinct);
+            }
+
+            string[] sortedDistinctArray = distinctList.OrderBy(s => s.Length).ToArray();
+            string shortestString = sortedDistinctArray[0];
+
+            foreach (char c in shortestString)
+            {
+                bool hasGemstone = true;
+
+                for (int n = 1; n < len; n++)
+                {
+                    hasGemstone = sortedDistinctArray[n].Contains(c.ToString());
+
+                    if (!hasGemstone)
+                    {
+                        break;
+                    }
+                }
+
+                if (hasGemstone)
+                {
+                    numberGemstones++;
+                }
+            }
+
+            return numberGemstones;
+        }
+    }
+}
