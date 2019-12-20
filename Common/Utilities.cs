83b1070fdfab891c6f937675122b34281181d33e
Frank J. Reashore <reashore@telus.net>
2019-12-20 14:18:15 -0800

    Solved SherlockAndArray problem

--- Common/Utilities.cs
+++ Common/Utilities.cs
@@ -9,16 +9,6 @@ namespace Common
 {
     public static class Utilities
     {
-        public static string[] ReadTextFile(string fileName)
-        {
-            string location = Assembly.GetExecutingAssembly().Location;
-            string rootDirectory = Path.GetDirectoryName(location);
-            string fullFileName = Path.Combine(rootDirectory, fileName);
-            string[] fileLines = File.ReadAllLines(fullFileName);
-
-            return fileLines;
-        }
-
         public static string Reverse(string s)
         {
             StringBuilder stringBuilder = new StringBuilder();
@@ -88,6 +78,109 @@ namespace Common
 
                 Console.WriteLine();
             }
+        }
+    }
+
+    public class TestCase
+    {
+        public List<int> Array;
+        public string Result;
+    }
+
+    public static class TestCaseUtilities
+    {
+        public  static void RunTestCases()
+        {
+            List<TestCase> testCaseList = GetTestCases();
+
+            for (int n = 0; n < testCaseList.Count; n++)
+            {
+                TestCase testCase = testCaseList[n];
+                List<int> array = testCase.Array;
+                string expectedResult = testCase.Result;
+                string result = Test(array);
+
+                if (result != expectedResult)
+                {
+                    Console.WriteLine($"Test case {n} failed, result = {result}, expectedResult = {expectedResult}");
+                }
+            }
+        }
+        private static string Test(List<int> array)
+        {
+            // replace with proper test function
+            return "YES";
+        }
+
+        private static List<TestCase> GetTestCases()
+        {
+            List<List<int>> testInputList = ReadTestInput();
+            List<string> testResultList = ReadTestResults();
+            List<TestCase> testCaseList = new List<TestCase>();
+            int testInputLen = testInputList.Count;
+            int testResultsLen = testResultList.Count;
+
+            if (testInputLen != testResultsLen)
+            {
+                throw new Exception("Test inputs and test results lengths are not equal");
+            }
+
+            for (int n = 0; n < testInputLen; n++)
+            {
+                TestCase testCase = new TestCase
+                {
+                    Array = testInputList[n],
+                    Result = testResultList[n]
+                };
+
+                testCaseList.Add(testCase);
+            }
+
+            return testCaseList;
+        }
+
+        public static string[] ReadTextFile(string fileName)
+        {
+            string location = Assembly.GetExecutingAssembly().Location;
+            string rootDirectory = Path.GetDirectoryName(location);
+            string fullFileName = Path.Combine(rootDirectory, fileName);
+            string[] fileLines = File.ReadAllLines(fullFileName);
+
+            return fileLines;
+        }
+
+        private static List<List<int>> ReadTestInput()
+        {
+            const string testInputFileName = @"TestData\TestInput.txt";
+            string[] testInputLines = TestCaseUtilities.ReadTextFile(testInputFileName);
+            List<List<int>> testInputList = new List<List<int>>();
+
+            int numberTestCases = Convert.ToInt32(testInputLines[0]);
+
+            for (int n = 0; n < numberTestCases; n++)
+            {
+                string arrayValuesString = testInputLines[n + 2];
+                string[] valueArray = arrayValuesString.Split(" ");
+                List<int> valuesList = new List<int>();
+
+                foreach (string valueString in valueArray)
+                {
+                    int value = Convert.ToInt32(valueString);
+                    valuesList.Add(value);
+                }
+
+                testInputList.Add(valuesList);
+            }
+
+            return testInputList;
+        }
+
+        private static List<string> ReadTestResults()
+        {
+            const string testResultsFileName = @"TestData\TestResults.txt";
+            string[] testResultLines = TestCaseUtilities.ReadTextFile(testResultsFileName);
+            List<string> testResultList = new List<string>(testResultLines);
+            return testResultList;
         }
     }
 }
---
+++ SherlockAndArrayProblem/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SherlockAndArrayProblem
+{
+    internal static class Program
+    {
+        internal static void Main()
+        {
+            RunSpecialTests();
+        }
+
+        private static void RunSpecialTests()
+        {
+            List<int> array = new List<int> { 1, 1, 4, 1, 1 };
+            string result = BalancedSums(array);                // YES
+            Console.WriteLine($"result = {result}");
+
+            array = new List<int> { 2, 0, 0, 0 };
+            result = BalancedSums(array);                        // YES
+            Console.WriteLine($"result = {result}");
+
+            array = new List<int> { 0, 0, 2, 0 };
+            result = BalancedSums(array);                        // YES
+            Console.WriteLine($"result = {result}");
+
+            array = new List<int> { 1, 2, 3 };
+            result = BalancedSums(array);                        // NO
+            Console.WriteLine($"result = {result}");
+
+            array = new List<int> { 1, 2, 3, 3 };
+            result = BalancedSums(array);                        // YES
+            Console.WriteLine($"result = {result}");
+
+            array = new List<int> { 1, 2, 3, 4, 6 };
+            result = BalancedSums(array);                        // YES
+            Console.WriteLine($"result = {result}");
+
+            array = new List<int> { 75, 26, 45, 72, 81, 47, 29, 97, 2, 75, 25, 82, 84, 17, 56, 32, 2, 28, 37, 57, 39, 18, 11, 79, 6, 40, 68, 68, 16, 40, 63, 93, 49, 91, 10, 55, 68, 31, 80, 57, 18, 34, 28, 76, 55, 21, 80, 22, 45, 11, 67, 67, 74, 91, 4, 35, 34, 65, 80, 21, 95, 1, 52, 25, 31, 2, 53, 96, 22, 89, 99, 7, 66, 32, 2, 68, 33, 75, 92, 84, 10, 94, 28, 54, 12, 9, 80, 43, 21, 51, 92, 20, 97, 7, 25, 67, 17, 38, 100, 86 };
+            result = BalancedSums(array);                        // NO
+            Console.WriteLine($"result = {result}");
+
+            array = new List<int> {83, 20, 6, 81, 58, 59, 53, 2, 54, 62, 25, 35, 79, 64, 27, 49, 32, 95, 100, 20, 58, 39, 92, 30, 67, 89, 58, 81, 100, 66, 73, 29, 75, 81, 70, 55, 18, 28, 7, 35, 98, 52, 30, 11, 69, 48, 84, 54, 13, 14, 15, 86, 34, 82, 92, 26, 8, 53, 62, 57, 50, 31, 61, 85, 88, 5, 80, 64, 90, 52, 47, 43, 40, 93, 69, 70, 16, 43, 7, 25, 99, 12, 63, 99, 71, 76, 55, 17, 90, 43, 27, 20, 42, 84, 39, 96, 75, 1, 58, 49};
+            result = BalancedSums(array);                        // NO
+            Console.WriteLine($"result = {result}");
+        }
+
+        private static string BalancedSums(List<int> array)
+        {
+            int arraySum = array.Sum(x => x);
+            int len = array.Count;
+
+            if (len == 1)
+            {
+                return "YES";
+            }
+
+            int rightSum = arraySum - array[0];
+            int leftSum = 0;
+
+            for (int n = 0; n < len - 1; n++)
+            {
+                int difference = rightSum - leftSum;
+
+                if (difference == 0)
+                {
+                    return "YES";
+                }
+                
+                if (difference < 0)
+                {
+                    break;
+                }
+
+                leftSum += array[n];
+                rightSum = arraySum - leftSum - array[n + 1];
+            }
+
+            return "NO";
+        }
+    }
+}
