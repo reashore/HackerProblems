7e786111741961862db892a45030a8a5c0b02029
Frank J. Reashore <reashore@telus.net>
2019-12-22 19:08:29 -0800

    Solved TowerBreakers problem

---
+++ TowerBreakersProblem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TowerBreakersProblem
+{
+    internal static class Program
+    {
+        internal static void Main()
+        {
+            int n = 2;
+            int m = 2;
+            int winningPlayer = TowerBreakers(n, m);                        // 2
+            Console.WriteLine($"winningPlayer = {winningPlayer}");
+
+            n = 1;
+            m = 4;
+            winningPlayer = TowerBreakers(n, m);                            // 1
+            Console.WriteLine($"winningPlayer = {winningPlayer}");
+        }
+
+        private static int TowerBreakers(int n, int m)
+        {
+            return (m == 1 || n % 2 == 0 ? 2 : 1);
+        }
+    }
+}
