162127a39421dd7a1489058ddeaa22b57796be09
Frank J. Reashore <reashore@telus.net>
2019-12-22 18:51:30 -0800

    Solved MaximizingXor problem

---
+++ MaximizingXorProblem/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MaximizingXorProblem
+{
+    internal static class Program
+    {
+        internal static void Main()
+        {
+            int low = 10;
+            int high = 15;
+            int maxXor = MaximizingXor(low, high);                  // 7
+            Console.WriteLine($"maxXor = {maxXor}");
+
+            low = 11;
+            high = 100;
+            maxXor = MaximizingXor(low, high);                      // 127
+            Console.WriteLine($"maxXor = {maxXor}");
+        }
+
+        private static int MaximizingXor(int low, int high)
+        {
+            int maxXor = 0;
+
+            for (int n1 = low; n1 <= high; n1++)
+            {
+                for (int n2 = low; n2 <= high; n2++)
+                {
+                    bool condition = n1 <= n2;
+
+                    if (!condition)
+                    {
+                        continue;
+                    }
+
+                    int xor = n1 ^ n2;
+
+                    if (xor > maxXor)
+                    {
+                        maxXor = xor;
+                    }
+                }
+            }
+
+            return maxXor;
+        }
+    }
+}
--- SumVersusXorProblem/Program.cs
+++ SumVersusXorProblem/Program.cs
@@ -33,7 +33,7 @@ namespace SumVersusXorProblem
             // The justification is that the xor simulates binary addition without the carry over to the next digit.
             // For the zero digits of n you can either add a 1 or 0 without getting a carry which implies xor = +
             // whereas if a digit in n is 1 then the matching digit in x is forced to be 0 on order to avoid carry.
-            // For each 0 in n in the matching digit in x can either being a 1 or 0 with a total combination count of 2 ^ (num of zero).
+            // For each 0 in n in the matching digit in x can either being a 1 or 0 with a total combination count of 2 ^ (num of zeros).
 
             long zeroCount = 0;
 
