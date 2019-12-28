33a248b27874eb80c48d8bc416d1392132b35d56
Frank J. Reashore <reashore@telus.net>
2019-12-27 16:02:57 -0800

    Solved AppendAndDelete problem

---
+++ AppendAndDeleteProblem/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace AppendAndDeleteProblem
+{
+    internal static class Program
+    {
+        internal static void Main()
+        {
+            string string1 = "hackerhappy";
+            string string2 = "hackerrank";
+            int numberOperations = 9;
+            string result = AppendAndDelete(string1, string2, numberOperations);        // Yes
+            Console.WriteLine($"result = {result}");
+
+            string1 = "aba";
+            string2 = "aba";
+            numberOperations = 7;
+            result = AppendAndDelete(string1, string2, numberOperations);               // Yes
+            Console.WriteLine($"result = {result}");
+
+            string1 = "ashley";
+            string2 = "ash";
+            numberOperations = 2;
+            result = AppendAndDelete(string1, string2, numberOperations);               // No
+            Console.WriteLine($"result = {result}");
+
+            string1 = "aaaaaaaaaa";
+            string2 = "aaaaa";
+            numberOperations = 7;
+            result = AppendAndDelete(string1, string2, numberOperations);               // Yes
+            Console.WriteLine($"result = {result}");
+
+            string1 = "zzzzz";
+            string2 = "zzzzzzz";
+            numberOperations = 4;
+            result = AppendAndDelete(string1, string2, numberOperations);               // Yes
+            Console.WriteLine($"result = {result}");
+
+            string1 = "asdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcv";
+            string2 = "asdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcv";
+            numberOperations = 20;
+            result = AppendAndDelete(string1, string2, numberOperations);               // Yes
+            Console.WriteLine($"result = {result}");
+        }
+
+        private static string AppendAndDelete(string string1, string string2, int numberOperations)
+        {
+            int commonLength = GetCommonLength(string1, string2);
+            int totalLength = string1.Length + string2.Length;
+            int totalMinusCommonLength = totalLength - 2 * commonLength;
+
+            // Case 1
+            if (totalMinusCommonLength > numberOperations)
+            {
+                return "No";
+            }
+
+            // Case 2
+            if ((totalMinusCommonLength - numberOperations) % 2 == 0)
+            {
+                return "Yes";
+            }
+
+            // Case 3
+            if (totalLength - numberOperations < 0)
+            {
+                return "Yes";
+            }
+
+            // Case 4
+            return "No";
+        }
+
+        private static int GetCommonLength(string s1, string s2)
+        {
+            int commonLength = 0;
+            int len1 = s1.Length;
+            int len2 = s2.Length;
+            int len = len1 >= len2 ? len2 : len1;
+
+            for (int n = 0; n < len; n++)
+            {
+                if (s1[n] == s2[n])
+                {
+                    commonLength++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return commonLength;
+        }
+    }
+}
---
+++ FindThe PointProblem/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindThe_PointProblem
+{
+    internal static class Program
+    {
+        internal static void Main()
+        {
+            int px;
+            int py;
+            int qx;
+            int qy;
+            int[] refelctionPoint;
+
+            px = 0;
+            py = 0;
+            qx = 1;
+            qy = 1;
+            refelctionPoint = FindPoint(px, py, qx, qy);
+            Print(refelctionPoint);
+            Console.WriteLine();
+
+            px = 1;
+            py = 1;
+            qx = 2;
+            qy = 2;
+            refelctionPoint = FindPoint(px, py, qx, qy);
+            Print(refelctionPoint);
+            Console.WriteLine();
+        }
+
+        private static void Print(IEnumerable<int> array)
+        {
+            string line = string.Join(", ", array);
+            Console.WriteLine(line);
+        }
+
+        private static int[] FindPoint(int px, int py, int qx, int qy)
+        {
+            int deltaX = qx - px;
+            int deltaY = qy - py;
+
+            int rx = qx + deltaX;
+            int ry = qy + deltaY;
+
+            return new[] {rx, ry};
+        }
+    }
+}
