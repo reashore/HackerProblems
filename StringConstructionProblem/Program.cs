33cef77eb34978e8b0adcad7d249297fe469d96b
Frank J. Reashore <reashore@telus.net>
2019-12-29 13:18:29 -0800

    Solved StringConstruction problem

---
+++ StringConstructionProblem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace StringConstructionProblem
+{
+    internal static class Program
+    {
+        internal static void Main()
+        {
+            string s = "abcd";
+            int cost = StringConstruction(s);                   // 4
+            Console.WriteLine($"cost = {cost}");
+
+            s = "abab";
+            cost = StringConstruction(s);                       // 2
+            Console.WriteLine($"cost = {cost}");
+        }
+
+        private static int StringConstruction(string s)
+        {
+            return s.Distinct().Count();
+        }
+    }
+}
