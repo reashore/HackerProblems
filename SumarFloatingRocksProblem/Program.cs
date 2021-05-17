ca2b8ef927ae6b4fbdf75c68bf0144eaac9f2219
Frank J. Reashore <reashore@telus.net>
2021-05-17 12:00:30 -0700

    Saving

--- SumarFloatingRocksProblem/Program.cs
+++ SumarFloatingRocksProblem/Program.cs
@@ -11,15 +11,23 @@ namespace SumarFloatingRocksProblem
     {
         internal static void Main()
         {
+            Test0();
             Test1();
             Test2();
+            WriteLine("New tests start");
+            Test3();
+            WriteLine("New tests end");
         }
 
         private static int Solve(int x1, int y1, int x2, int y2)
         {
+            // vertical line
             if (x1 == x2)
             {
-                WriteLine("Line is vertical");
+                if (y1 < y2)
+                {
+                    Swap(ref y1, ref y2);
+                }
 
                 int difference = y1 - y2 - 1;
 
@@ -31,29 +39,31 @@ namespace SumarFloatingRocksProblem
                 return difference;
             }
 
-
             if (x1 > x2)
             {
                 Swap(ref x1, ref x2);
                 Swap(ref y1, ref y2);
             }
 
-            double xd1 = x1;
-            double xd2 = x2;
-            double yd1 = y1;
-            double yd2 = y2;
-            double slope = (yd1 - yd2) / (xd1 - xd2);
-            double xMean = (xd1 + xd2) / 2;
-            double yMean = (yd1 + yd2) / 2;
+            //int deltaX = x2 - x1;
+            //int deltaY = y2 - y1;
+
+            //if (deltaY % deltaX != 0)
+            //{
+            //    return 0;
+            //}
+
+            double slope = (double)(y1 - y2) / (x1 - x2);
+            double xMean = (double)(x1 + x2) / 2;
+            double yMean = (double)(y1 + y2) / 2;
+            double offset = yMean - slope * xMean;
             int count = 0;
             
             for (int x = x1 + 1; x < x2; x++)
             {
-                double y = GetLinearValue(x, slope, xMean, yMean);
+                double y = slope * x + offset;
 
-                //WriteLine($"x = {x}, y = {y}");
-
-                if (IsAppoximatelyEqualToInteger(y))
+                if (IsEqualToInteger(y))
                 {
                     count++;
                 }
@@ -69,29 +79,21 @@ namespace SumarFloatingRocksProblem
             x2 = temp;
         }
 
-        private static double GetLinearValue(double x, double slope, double xMean, double yMean)
+        private static bool IsEqualToInteger(double value, double delta = .00000000001)
         {
-            double y = slope * (x - xMean) + yMean;
-            return y;
-        }
-
-        private static bool IsAppoximatelyEqualToInteger(double value, double delta = .000001)
-        {
-            double roundedValue = Math.Round(value);
-            double difference = value - roundedValue;
+            double difference = value - Math.Round(value);
 
             if (difference < 0)
             {
                 difference = -difference;
             }
 
-            bool result = difference < delta;
-            return result;
+            return difference < delta;
         }
 
         #region Test code
 
-        private static void Test1()
+        private static void Test0()
         {
             int x1 = 0;
             int y1 = 2;
@@ -114,41 +116,57 @@ namespace SumarFloatingRocksProblem
             count = Solve(x1, y1, x2, y2);
             WriteLine($"count = {count}");
 
-            x1 = 21;
-            y1 = -87;
-            x2 = -81;
-            y2 = -33;
-            count = Solve(x1, y1, x2, y2);
-            WriteLine($"count = {count}");
-
-            x1 = -89;
-            y1 = -89;
-            x2 = -8;
-            y2 = 10;
-            count = Solve(x1, y1, x2, y2);
+            // actual = 2, expected = 0: x1 = 7173287, y1 = -8172527, x2 = 5556293, y2 = 7032662
+            x1 = 7173287;
+            y1 = -8172527;
+            x2 = 5556293;
+            y2 = 7032662;
+            count = Solve(x1, y1, x2, y2);   // expected 0
             WriteLine($"count = {count}");
 
             WriteLine();
         }
 
+        private static void Test1()
+        {
+            const string testDataFileName = @"./TestData1.txt";
+            const string expectedValuesFileName = @"./ExpectedValues1.txt";
+
+            Test(testDataFileName, expectedValuesFileName);
+            WriteLine();
+        }
+
         private static void Test2()
         {
-            string[] lines = GetTestData();
-            List<int> expectedList = GetExpectedValues();
+            const string testDataFileName = @"./TestData2.txt";
+            const string expectedValuesFileName = @"./ExpectedValues2.txt";
+
+            Test(testDataFileName, expectedValuesFileName);
+            WriteLine();
+        }
+
+        private static void Test3()
+        {
+            const string testDataFileName = @"./TestData3.txt";
+            const string expectedValuesFileName = @"./ExpectedValues3.txt";
+
+            Test(testDataFileName, expectedValuesFileName);
+            WriteLine();
+        }
+
+        private static void Test(string testDataFileName, string expectedValuesFileName)
+        {
+            string[] lines = GetTestData(testDataFileName);
+            List<int> expectedList = GetExpectedValues(expectedValuesFileName);
 
             for (int n = 0; n < lines.Length; n++)
             {
                 string[] values = lines[n].Split(" ");
 
-                string x1String = values[0];
-                string y1String = values[1];
-                string x2String = values[2];
-                string y2String = values[3];
-
-                int.TryParse(x1String, out int x1);
-                int.TryParse(y1String, out int y1);
-                int.TryParse(x2String, out int x2);
-                int.TryParse(y2String, out int y2);
+                int.TryParse(values[0], out int x1);
+                int.TryParse(values[1], out int y1);
+                int.TryParse(values[2], out int x2);
+                int.TryParse(values[3], out int y2);
 
                 int actual = Solve(x1, y1, x2, y2);
                 int expected = expectedList[n];
@@ -157,23 +175,17 @@ namespace SumarFloatingRocksProblem
                 {
                     WriteLine($"Failed: actual = {actual}, expected = {expected}: x1 = {x1}, y1 = {y1}, x2 = {x2}, y2 = {y2}");
                 }
-                else
-                {
-                    WriteLine("Passed");
-                }
             }
         }
 
-        private static string[] GetTestData()
+        private static string[] GetTestData(string testDataFileName)
         {
-            const string testDataFileName = @"./TestData1.txt";
             string[] lines = ReadTextFile(testDataFileName);
             return lines;
         }
 
-        private static List<int> GetExpectedValues()
+        private static List<int> GetExpectedValues(string expectedValuesFileName)
         {
-            const string expectedValuesFileName = @"./ExpectedValues1.txt";
             string[] expectedStringValues = ReadTextFile(expectedValuesFileName);
 
             List<int> expectedValues = new List<int>();
