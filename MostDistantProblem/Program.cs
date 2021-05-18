433dcd2e58c10704628b0d64422c5d0cd3f725b5
Frank J. Reashore <reashore@telus.net>
2021-05-17 23:39:43 -0700

    Saving

---
+++ MostDistantProblem/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace MostDistantProblem
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    internal class Program
+    {
+        internal static void Main()
+        {
+            List<List<int>> pointList = new()
+            {
+                new List<int>() {-1, 0},
+                new List<int>() {1, 0},
+                new List<int>() {0, 1},
+                new List<int>() {0, -1},
+            };
+            double maximumDistance = Solve(pointList);
+            WriteLine($"maximumDistance = {maximumDistance}");
+        }
+
+        private static double Solve(List<List<int>> points)
+        {
+            List<Point> pointList = ConvertToListOfPoints(points);
+            int minX = int.MaxValue;
+            int maxX = int.MinValue;
+            int minY = int.MaxValue;
+            int maxY = int.MinValue;
+
+            foreach (Point point in pointList)
+            {
+                int x = point.X;
+                int y = point.Y;
+
+                if (x < minX)
+                {
+                    minX = x;
+                }
+
+                if (x > maxX)
+                {
+                    maxX = x;
+                }
+
+                if (y < minY)
+                {
+                    minY = y;
+                }
+
+                if (y > maxY)
+                {
+                    maxY = y;
+                }
+            }
+
+            int distance1 = maxX - minX;
+            int distance2 = maxY - minY;
+
+            return distance1 > distance2 ? distance1 : distance2;
+        }
+
+
+        #region solution 1
+
+        private static double Solve2(List<List<int>> points)
+        {
+            List<Point> pointList = ConvertToListOfPoints(points);
+            double maxDistance = 0;
+            Point maxPoint1 = new Point();
+            Point maxPoint2 = new Point();
+
+            for (int n1 = 0; n1 < pointList.Count; n1++)
+            {
+                for (int n2 = 0; n2 < pointList.Count; n2++)
+                {
+                    if (n1 == n2)
+                    {
+                        continue;
+                    }
+
+                    Point p1 = pointList[n1];
+                    Point p2 = pointList[n2];
+
+                    double distance = GetDistance1(p1, p2);
+
+                    if (distance > maxDistance)
+                    {
+                        maxDistance = distance;
+                        maxPoint1 = p1;
+                        maxPoint2 = p2;
+                    }
+                }
+            }
+
+            maxDistance = GetDistance2(maxPoint1, maxPoint2);
+            
+            return maxDistance;
+        }
+
+        private static List<Point> ConvertToListOfPoints(List<List<int>> points)
+        {
+            List<Point> pointList = new List<Point>();
+
+            foreach (List<int> point in points)
+            {
+                int x = point[0];
+                int y = point[1];
+                Point p = new Point(x, y);
+                pointList.Add(p);
+            }
+
+            return pointList;
+        }
+
+        private static double GetDistance1(Point p1, Point p2)
+        {
+            int x1 = p1.X;
+            int y1 = p1.Y;
+            int x2 = p2.X;
+            int y2 = p2.Y;
+
+            return Abs(x1 - x2) + Abs(y1 - y2);
+        }
+
+        private static double GetDistance2(Point p1, Point p2)
+        {
+            int x1 = p1.X;
+            int y1 = p1.Y;
+            int x2 = p2.X;
+            int y2 = p2.Y;
+
+            return Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+        }
+
+        private static int Abs(int x)
+        {
+            if (x > 0)
+            {
+                return x;
+            }
+
+            return -x;
+        }
+
+        #endregion
+    }
+
+    public class Point
+    {
+        public Point()
+        {
+
+        }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; }
+        public int Y { get; }
+    }
+}
---
+++ RussianPeasantExponentiationProblem/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+using static System.Console;
+
+namespace RussianPeasantExponentiationProblem
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    internal class Program
+    {
+        internal static void Main()
+        {
+            int real = 2;
+            int imaginary = 0;
+            int exponent = 9;
+            int modulo = 1000;
+            List<int> result = Solve(real, imaginary, exponent, modulo);
+            int realPart = result[0];
+            int imaginaryPart = result[1];
+            WriteLine($"{realPart} {imaginaryPart}");
+
+            real = 0;
+            imaginary = 1;
+            exponent = 5;
+            modulo = 10;
+            result = Solve(real, imaginary, exponent, modulo);
+            realPart = result[0];
+            imaginaryPart = result[1];
+            WriteLine($"{realPart} {imaginaryPart}");
+
+            real = 8;
+            imaginary = 2;
+            exponent = 10;
+            modulo = 1_000_000_000;
+            result = Solve(real, imaginary, exponent, modulo);
+            realPart = result[0];
+            imaginaryPart = result[1];
+            WriteLine($"{realPart} {imaginaryPart}");
+        }
+
+        private static List<int> Solve(int real, int imaginary, long exponent, int modulo)
+        {
+            Complex complex = new Complex(real, imaginary);
+            Complex exponentiatedComplex = Complex.Pow(complex, exponent);
+
+            double realPart1 = exponentiatedComplex.Real;
+            double imaginaryPart1 = exponentiatedComplex.Imaginary;
+
+            int realPart2 = (int) realPart1 % modulo;
+            int imaginaryPart2 = (int) imaginaryPart1 % modulo;
+
+            List<int> result = new List<int>();
+            result.Add(realPart2);
+            result.Add(imaginaryPart2);
+
+            return result;
+        }
+    }
+}
