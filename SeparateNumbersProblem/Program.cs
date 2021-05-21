e8ad49163ebaa07d7ad130c6094e0e47c0030a30
Frank J. Reashore <reashore@telus.net>
2021-05-20 22:15:14 -0700

    Saving

--- SeparateNumbersProblem/Program.cs
+++ SeparateNumbersProblem/Program.cs
@@ -27,10 +27,12 @@ namespace SeparateNumbersProblem
             number = "9899100";
             SeparateNumbers(number);                    // YES 98
 
+            number = "01";
+            SeparateNumbers(number);                    // YES 0
+
             number = "1";
             SeparateNumbers(number);                    // NO
 
-            Console.WriteLine("###########################################");
             number = "999100010001";
             SeparateNumbers(number);                    // NO
 
@@ -41,7 +43,7 @@ namespace SeparateNumbersProblem
             SeparateNumbers(number);                    // NO
 
             number = "13";
-            SeparateNumbers(number);                    // NO
+            SeparateNumbers(number);                    // NO (fails)
         }
 
         private static void SeparateNumbers(string number)
@@ -59,7 +61,7 @@ namespace SeparateNumbersProblem
                 Console.WriteLine("NO");
                 return;
             }
-            
+
             while (true)
             {
                 bool firstTimeFlag = true;
@@ -72,7 +74,9 @@ namespace SeparateNumbersProblem
 
                     if (loopNumberLength > remainderNumber.Length)
                     {
-                        Console.WriteLine("Breaking 1");
+                        Console.WriteLine("Breaking 1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                        //Console.WriteLine("NO");
+                        //return;
                         break;
                     }
 
@@ -86,7 +90,7 @@ namespace SeparateNumbersProblem
                     }
 
                     //-------------------------------------
-                    
+
                     // is this guaranteed to succeed?
                     remainderNumber = remainderNumber.Substring(loopNumberLength);
 
@@ -94,6 +98,9 @@ namespace SeparateNumbersProblem
                     {
                         match = true;
                         Console.WriteLine("Breaking 2");
+                        //Console.WriteLine("NO");
+                        //return;
+
                         break;
                     }
 
@@ -105,7 +112,8 @@ namespace SeparateNumbersProblem
                     if (loopNumberLength > remainderNumber.Length)
                     {
                         Console.WriteLine($"Breaking 3: loopNumberLength = {loopNumberLength}, remainderNumber = {remainderNumber}");
-                        break;
+                        Console.WriteLine("NO");
+                        return;
                     }
 
                     string actualNextNumber = remainderNumber.Substring(0, expectedNextNumber.Length);
@@ -117,7 +125,6 @@ namespace SeparateNumbersProblem
                     if (actualNextNumber != expectedNextNumber)
                     {
                         Console.WriteLine("Breaking 4");
-
                         break;
                     }
 
@@ -144,6 +151,112 @@ namespace SeparateNumbersProblem
             var message = match ? $"YES {firstNumber}" : "NO";
             Console.WriteLine(message);
         }
+
+        //private static void SeparateNumbers(string number)
+        //{
+        //    bool match = false;
+        //    int firstNumber = 0;
+        //    int numberLength = 1;
+
+        //    Console.WriteLine("*************************************");
+
+        //    Console.WriteLine($"number = {number}");
+
+        //    if (number.Length <= 1)
+        //    {
+        //        Console.WriteLine("NO");
+        //        return;
+        //    }
+
+        //    while (true)
+        //    {
+        //        bool firstTimeFlag = true;
+        //        int loopNumberLength = numberLength;
+        //        string remainderNumber = number;
+
+        //        while (remainderNumber.Length > 0)
+        //        {
+        //            //-------------------------------------
+
+        //            if (loopNumberLength > remainderNumber.Length)
+        //            {
+        //                Console.WriteLine("Breaking 1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        //                //Console.WriteLine("NO");
+        //                //return;
+        //                break;
+        //            }
+
+        //            string currentNumber = remainderNumber.Substring(0, loopNumberLength);
+        //            int currentNumberInt = GetNumber(currentNumber);
+
+        //            if (firstTimeFlag)
+        //            {
+        //                firstNumber = currentNumberInt;
+        //                firstTimeFlag = false;
+        //            }
+
+        //            //-------------------------------------
+
+        //            // is this guaranteed to succeed?
+        //            remainderNumber = remainderNumber.Substring(loopNumberLength);
+
+        //            if (remainderNumber == "")
+        //            {
+        //                match = true;
+        //                Console.WriteLine("Breaking 2");
+        //                //Console.WriteLine("NO");
+        //                //return;
+
+        //                break;
+        //            }
+
+        //            string expectedNextNumber = GetString(currentNumberInt + 1);
+        //            loopNumberLength = expectedNextNumber.Length;
+
+        //            //-------------------------------------
+
+        //            if (loopNumberLength > remainderNumber.Length)
+        //            {
+        //                Console.WriteLine($"Breaking 3: loopNumberLength = {loopNumberLength}, remainderNumber = {remainderNumber}");
+        //                Console.WriteLine("NO");
+        //                return;
+        //            }
+
+        //            string actualNextNumber = remainderNumber.Substring(0, expectedNextNumber.Length);
+
+        //            //-------------------------------------
+
+        //            Console.WriteLine($"firstNumber = {firstNumber}, currentNumber = {currentNumber}, expectedNextNumber = {expectedNextNumber}, actualNextNumber = {actualNextNumber}, remainder = {remainderNumber}");
+
+        //            if (actualNextNumber != expectedNextNumber)
+        //            {
+        //                Console.WriteLine("Breaking 4");
+        //                break;
+        //            }
+
+        //            if (remainderNumber == "")
+        //            {
+        //                Console.WriteLine("Breaking 5");
+        //                match = true;
+        //            }
+        //        }
+
+        //        if (match)
+        //        {
+        //            break;
+        //        }
+
+        //        if (numberLength > 5)
+        //        {
+        //            break;
+        //        }
+
+        //        numberLength++;
+        //    }
+
+        //    var message = match ? $"YES {firstNumber}" : "NO";
+        //    Console.WriteLine(message);
+        //}
 
         private static int GetNumber(string numberString)
         {
