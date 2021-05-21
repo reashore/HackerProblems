34dc491cc5b2fc84831ca52d4ec082eb6d7bd4ad
Frank J. Reashore <reashore@telus.net>
2021-05-21 10:30:52 -0700

    Solved ConstructingNumber problem

---
+++ ConstructingNumberProblem/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using static System.Console;
+
+namespace ConstructingNumberProblem
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    internal class Program
+    {
+        internal static void Main()
+        {
+            List<int> numberList = new List<int>()
+            {
+                9
+            };
+            string result = CanConstruct(numberList);
+            WriteLine($"Result = {result}");
+
+            numberList = new List<int>()
+            {
+                40,
+                50,
+                90
+            };
+            result = CanConstruct(numberList);
+            WriteLine($"Result = {result}");
+
+            numberList = new List<int>()
+            {
+                1,
+                4
+            };
+            result = CanConstruct(numberList);
+            WriteLine($"Result = {result}");
+        }
+
+        private static string CanConstruct(List<int> numberList)
+        {
+            ulong sum = 0;
+
+            foreach (int number in numberList)
+            {
+                sum += (ulong)number % 3;
+            }
+
+            string message = sum % 3 == 0 ? "Yes" : "No";
+            return message;
+        }
+    }
+}
