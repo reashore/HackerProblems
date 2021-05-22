f3393a806677be2a2ba228ff18becad28a361810
Frank J. Reashore <reashore@telus.net>
2021-05-21 20:51:04 -0700

    Solved SumarFloatingRocks problem

--- Common/Utilities.cs
+++ Common/Utilities.cs
@@ -142,5 +142,22 @@ namespace Common
                 Console.WriteLine();
             }
         }
+        
+        private static int Gcf(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        private static int Lcm(int a, int b)
+        {
+            return (a / Gcf(a, b)) * b;
+        }
+
     }
 }
--- SumarFloatingRocksProblem/Program.cs
+++ SumarFloatingRocksProblem/Program.cs
@@ -11,139 +11,55 @@ namespace SumarFloatingRocksProblem
     {
         internal static void Main()
         {
-            int lcm1 = Lcm(2, 4);
-            int lcm2 = Lcm(3, 9);
-
-            int gcf1 = Gcf(2, 4);
-            int gcf2 = Gcf(3, 9);
-            int gcf3 = Gcf(3, 3);
-
             Test0();
             Test1();
             Test2();
-            //WriteLine("New tests start");
-            //Test3();
-            //WriteLine("New tests end");
+            Test3();
         }
-
-        //private static int Solve(int x1, int y1, int x2, int y2)
-        //{
-        //    // vertical line
-        //    if (x1 == x2)
-        //    {
-        //        if (y1 < y2)
-        //        {
-        //            Swap(ref y1, ref y2);
-        //        }
-
-        //        int difference = y1 - y2 - 1;
-
-        //        if (difference < 0)
-        //        {
-        //            difference = -difference;
-        //        }
-
-        //        return difference;
-        //    }
-
-        //    if (x1 > x2)
-        //    {
-        //        Swap(ref x1, ref x2);
-        //        Swap(ref y1, ref y2);
-        //    }
-
-        //    int deltaX = x2 - x1;
-        //    int deltaY = y2 - y1;
-
-        //    if (deltaX < 0)
-        //    {
-        //        deltaX = -deltaX;
-        //    }
-
-        //    if (deltaY < 0)
-        //    {
-        //        deltaY = -deltaY;
-        //    }
-
-        //    if (deltaY > deltaX)
-        //    {
-        //        Swap(ref deltaX, ref deltaY);
-        //    }
-
-        //    int gcf = Gcf(deltaX, deltaY);
-
-        //    int count = deltaX / gcf - 1;
-
-        //    return count;
-        //}
 
         private static int Solve(int x1, int y1, int x2, int y2)
         {
             // vertical line
             if (x1 == x2)
             {
-                WriteLine("Vertical line");
-
-                if (y1 < y2)
-                {
-                    Swap(ref y1, ref y2);
-                }
-
-                int difference = y1 - y2 - 1;
+                int difference = y1 - y2;
 
                 if (difference < 0)
                 {
                     difference = -difference;
                 }
 
-                return difference;
+                return difference - 1;
             }
 
             // horizontal line
             if (y1 == y2)
             {
-                WriteLine("Horizontal line");
-
-                if (y1 < y2)
-                {
-                    Swap(ref y1, ref y2);
-                }
-
-                int difference = y1 - y2 - 1;
+                int difference = x1 - x2;
 
                 if (difference < 0)
                 {
                     difference = -difference;
                 }
 
-                return difference;
+                return difference - 1;
             }
 
-            if (x1 > x2)
-            {
-                Swap(ref x1, ref x2);
-                Swap(ref y1, ref y2);
-            }
+            int deltaX = Abs(x2 - x1);
+            int deltaY = Abs( y2 - y1);
 
-            int deltaX = x2 - x1;
-            int deltaY = y2 - y1;
-            double slope = (double)deltaY / deltaX;
-            double xMean = (double)(x1 + x2) / 2;
-            double yMean = (double)(y1 + y2) / 2;
-            double offset = yMean - slope * xMean;
-            int count = 0;
-
-            for (int x = x1 + 1; x < x2; x++)
-            {
-                double y = slope * x + offset;
-
-                if (IsEqualToInteger(y))
-                {
-                    count++;
-                }
-            }
-
+            int count = Gcf(deltaY, deltaX) - 1;
             return count;
+        }
+
+        private static int Abs(int x)
+        {
+            if (x < 0)
+            {
+                return -x;
+            }
+
+            return x;
         }
 
         private static int Gcf(int a, int b)
@@ -155,30 +71,6 @@ namespace SumarFloatingRocksProblem
                 a = temp;
             }
             return a;
-        }
-
-        private static int Lcm(int a, int b)
-        {
-            return (a / Gcf(a, b)) * b;
-        }
-
-        private static void Swap(ref int x1, ref int x2)
-        {
-            int temp = x1;
-            x1 = x2;
-            x2 = temp;
-        }
-
-        private static bool IsEqualToInteger(double value, double delta = .00000000001)
-        {
-            double difference = value - Math.Round(value);
-
-            if (difference < 0)
-            {
-                difference = -difference;
-            }
-
-            return difference < delta;
         }
 
         #region Test code
@@ -206,21 +98,21 @@ namespace SumarFloatingRocksProblem
             count = Solve(x1, y1, x2, y2);                  // 6
             WriteLine($"count = {count}");
 
-            //x1 = 7173287;
-            //y1 = -8172527;
-            //x2 = 5556293;
-            //y2 = 7032662;
-            //count = Solve(x1, y1, x2, y2);   // expected 0
-            //WriteLine($"count = {count}");
+            x1 = 7173287;
+            y1 = -8172527;
+            x2 = 5556293;
+            y2 = 7032662;
+            count = Solve(x1, y1, x2, y2);                  // 0
+            WriteLine($"count = {count}");
 
-            //x1 = 7158191;
-            //y1 = -5226453;
-            //x2 = -4407451;
-            //y2 = 8512618;
-            //count = Solve(x1, y1, x2, y2);   // expected 18
-            //WriteLine($"count = {count}");
+            x1 = 7158191;
+            y1 = -5226453;
+            x2 = -4407451;
+            y2 = 8512618;
+            count = Solve(x1, y1, x2, y2);                  // 18
+            WriteLine($"count = {count}");
 
-            //WriteLine();
+            WriteLine();
         }
 
         private static void Test1()
