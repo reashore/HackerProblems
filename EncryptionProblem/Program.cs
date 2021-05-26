3613435746ebd57d5160677a4c2675344c5596d7
Frank J. Reashore <reashore@telus.net>
2021-05-26 00:30:02 -0700

    Solved Encryption problem

--- EncryptionProblem/Program.cs
+++ EncryptionProblem/Program.cs
@@ -51,13 +51,12 @@ namespace EncryptionProblem
             double sqrtLength = Math.Sqrt(length);
             int rows = (int)Math.Floor(sqrtLength);
             int cols = (int)Math.Ceiling(sqrtLength);
+            StringBuilder stringBuilder = new StringBuilder();
 
             if (rows * cols < length)
             {
                 rows++;
             }
-
-            StringBuilder stringBuilder = new StringBuilder();
 
             for (int col = 0; col < cols; col++)
             {
@@ -76,128 +75,5 @@ namespace EncryptionProblem
 
             return stringBuilder.ToString().Trim();
         }
-
-        //private static string Encryption(string value)
-        //{
-        //    value = value.Replace(" ", "");
-        //    int length = value.Length;
-        //    (int rows, int cols) = GetRowsAndCols(length);
-        //    StringBuilder stringBuilder = new StringBuilder();
-
-        //    //WriteLine($"rows = {rows}, cols = {cols}");
-
-        //    for (int col = 0; col < cols; col++)
-        //    {
-        //        for (int row = 0; row < rows; row++)
-        //        {
-        //            int index = row + col + (cols - 1) * row;
-
-        //            if (index < length)
-        //            {
-        //                stringBuilder.Append(value[index]);
-        //            }
-
-        //            //WriteLine($"rows = {rows}, cols = {cols}, row = {row}, col = {col}, index = {index}");
-        //        }
-
-        //        stringBuilder.Append(" ");
-        //    }
-
-        //    return stringBuilder.ToString().Trim();
-        //}
-
-        private static string RemoveSpaces(string value)
-        {
-            StringBuilder stringBuilder = new StringBuilder();
-
-            foreach (char character in value)
-            {
-                if (character != ' ')
-                {
-                    stringBuilder.Append(character);
-                }
-            }
-
-            return stringBuilder.ToString();
-        }
-
-        private static void GetRowsAndCols(int length, out int rows, out int cols)
-        {
-            double sqrtLength = Math.Sqrt(length);
-            rows = (int)Math.Floor(sqrtLength);
-            cols = (int)Math.Ceiling(sqrtLength);
-
-            if (rows * cols < length)
-            {
-                rows++;
-            }
-
-            //return (rows, cols);
-        }
-
-        //private static (int, int) GetRowsAndCols(int length)
-        //{
-        //    double sqrtLength = Math.Sqrt(length);
-
-        //    int rows = (int)Math.Floor(sqrtLength);
-        //    int cols = (int)Math.Ceiling(sqrtLength);
-
-        //    if (rows * cols < length)
-        //    {
-        //        rows++;
-        //        return (rows, cols);
-        //    }
-        //    return (rows, cols);
-
-
-
-        //    int row1 = rows - 1;
-        //    int row2 = rows + 1;
-
-        //    int col1 = cols - 1;
-        //    int col2 = cols + 1;
-
-        //    // Choose row and col such that:
-        //    // 1) row x col > length and
-        //    // 2) area = row x col is minimum
-
-        //    int minArea = int.MaxValue;
-        //    int minRow = 0;
-        //    int minCol = 0;
-
-        //    for (int row = row1; row < row2; row++)
-        //    {
-        //        for (int col = col1; col < col2; col++)
-        //        {
-        //            int area = row * col;
-
-        //            if (area < length)
-        //            {
-        //                continue;
-        //            }
-
-        //            if (area < minArea)
-        //            {
-        //                minArea = area;
-        //                minRow = row;
-        //                minCol = col;
-        //            }
-        //        }
-        //    }
-
-        //    //if (minRow * minCol < length)
-        //    //{
-        //    //    WriteLine($"maxRow x maxCol = {minRow}x{minCol} < {length}");
-        //    //    throw new Exception("maxRows * maxCol > length");
-        //    //}
-
-        //    if (minRow >= minCol)
-        //    {
-        //        WriteLine($"maxRow = {minRow}, cols = {minCol}");
-        //        throw new Exception("maxRow >= maxCol");
-        //    }
-
-        //    return (minRow, minCol);
-        //}
     }
 }
